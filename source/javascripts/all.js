a4e5b59d8ea431443c64d6b35e5de443f2b5a406
Raoul DIFFOUO <diraulo@gmail.com>
2016-09-25 11:37:55 +0200

    implement smooth scroll when click on nav link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,19 @@
 //= require bootstrap.min
 //= require material.min
 //= require material-kit
+
+function smoothScroll(target) {
+  $('body,html').animate({
+    'scrollTop': target.offset().top
+  },
+  900);
+}
+
+function goto(target) {
+  event.preventDefault();
+  smoothScroll($(target));
+}
+
+$('#contact-link').on('click', function() {
+  goto(this.hash);
+});
