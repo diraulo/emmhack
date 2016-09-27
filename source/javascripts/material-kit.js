dc7a3f7452a614f48ec9de55533aea8babc4ee20
Raoul DIFFOUO <diraulo@gmail.com>
2016-09-28 00:38:02 +0200

    update hackathon venue and contact details

--- source/javascripts/material-kit.js
+++ source/javascripts/material-kit.js
@@ -125,16 +125,16 @@ materialKitDemo = {
     }, 4),
 
     initContactUs2Map: function(){
-      var lat = -26.1826453;
-      var long = 28.1853571;
+      var lat = -26.265658; // -26.1826453;
+      var long = 28.123249; // 28.1853571;
 
-      var centerLong = long - 0.025;
+      var centerLong = long - 0.014;
 
       var myLatlng = new google.maps.LatLng(lat,long);
       var centerPosition = new google.maps.LatLng(lat, centerLong);
 
       var mapOptions = {
-        zoom: 14,
+        zoom: 15,
         center: centerPosition,
         styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
         scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
